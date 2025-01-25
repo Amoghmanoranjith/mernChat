1551fdc5dea0036a039b0a7e84208373068adc4f
rishibakshi <guitarguyrishi@gmail.com>
2025-01-25 20:51:18 +0530

    chore: remove un-used custom hooks

--- next-js-frontend/src/hooks/userRequest/useFetchFriendRequest.ts
+++
@@ -1,8 +0,0 @@
-import { useGetUserFriendRequestsQuery } from "../../services/api/requestApi"
-import { useToast } from "../useUI/useToast"
-
-export const useFetchFriendRequest = () => {
-
-    const {error,isError,isFetching,isSuccess,isUninitialized} = useGetUserFriendRequestsQuery()
-    useToast({error,isError,isLoading:isFetching,isSuccess,isUninitialized})
-}
