f35c35cae5a8e589deec2767b06412764c608d82
rishibakshi <guitarguyrishi@gmail.com>
2025-02-05 11:05:33 +0530

    feat: implement prisma in my user controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,17 +1,17 @@
+import { UploadApiResponse } from "cloudinary";
 import { NextFunction, Response } from "express";
 import type { AuthenticatedRequest } from "../interfaces/auth/auth.interface.js";
-import { CustomError, asyncErrorHandler } from "../utils/error.utils.js";
-import { User } from "../models/user.model.js";
-import { deleteFilesFromCloudinary, getSecureUserInfo, uploadFilesToCloudinary } from "../utils/auth.util.js";
-import type { IUser } from "../interfaces/auth/auth.interface.js";
+import { prisma } from "../lib/prisma.lib.js";
 import { notificationsSchemaType } from "../schemas/user.schema.js";
+import { deleteFilesFromCloudinary, uploadFilesToCloudinary } from "../utils/auth.util.js";
 import { sendMail } from "../utils/email.util.js";
+import { CustomError, asyncErrorHandler } from "../utils/error.utils.js";
 
-const getUserDetails = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
+export const getUserDetails = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
     return res.status(200).json(req.user)
 })
 
-const getUserByUsername = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
+export const getUserByUsername = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
 
     const {username} = req.query
 
@@ -19,106 +19,117 @@ const getUserByUsername = asyncErrorHandler(async(req:AuthenticatedRequest,res:R
         return next(new CustomError("Username cannot be empty"))
     }
 
-    const regexUsername = new RegExp(username.toString().trim(), "i")
+    const searchTerm = username.toString().trim();
 
-    const transformedResults = await User.aggregate([
-        {
-            $match:{
-                username:{
-                    $regex:regexUsername
-                }
+    const users = await prisma.user.findMany({
+        where:{
+            username:{
+                contains:searchTerm,
+                mode:"insensitive"
             }
         },
-        {
-            $addFields:{
-                avatar:"$avatar.secureUrl"
-            }
-        },
-        {
-            $project:{
-                name:1,
-                username:1,
-                avatar:1
-            }
+        select:{
+            id:true,
+            name:true,
+            username:true,
+            avatar:true
         }
-    ])
+    })
 
-    return res.status(200).json(transformedResults)
-    
+    return res.status(200).json(users)
 })
 
-const udpateUser  = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
-    
-    if(req.file){ 
-        let uploadResults
-        const existingPublicId = req.user?.avatar?.publicId
-
-        if(!existingPublicId){
-            uploadResults = await uploadFilesToCloudinary([req.file])
-        }
-        else if(existingPublicId){
-
-            const avatarPromise = [
-                deleteFilesFromCloudinary([existingPublicId]),
-                uploadFilesToCloudinary([req.file])
-            ]
-
-            const [_,result] = await Promise.all(avatarPromise)
-            uploadResults = result
-            
-        }
-
-        if(uploadResults){
-
-            const updatedUser = await User.findByIdAndUpdate(req.user?._id,{
-                avatar:{
-                    secureUrl:uploadResults[0].secure_url,
-                    publicId:uploadResults[0].public_id
-                }
-            },{new:true}) as IUser
-
-            return res.status(200).json(getSecureUserInfo(updatedUser))
-        }
-
-        return next(new CustomError("Some error occured",500))
+export const udpateUser  = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
         
-    }
-})
+        if(!req.file){
+            return next(new CustomError("Please provide an image",400))
+        }
 
-const updateNotifications = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
+        let uploadResults:UploadApiResponse[] | undefined
+        const existingAvatarPublicId = req.user.avatarCloudinaryPublicId
+
+        if(!existingAvatarPublicId){
+            uploadResults = await uploadFilesToCloudinary({files:[req.file]})
+            if(!uploadResults){
+                return next(new CustomError("Some error occured",500))
+            }
+        }
+        else{
+            const cloudinaryFilePromises = [
+                deleteFilesFromCloudinary({publicIds:[existingAvatarPublicId]}),
+                uploadFilesToCloudinary({files:[req.file]})
+            ]
+            const [_,result] = await Promise.all(cloudinaryFilePromises) as [any,UploadApiResponse[] | undefined]
+            if(!result) return next(new CustomError("Some error occured",500))
+            uploadResults = result
+        }
+        
+        const user = await prisma.user.update({
+            where:{
+                id:req.user.id
+            },
+            data:{
+                avatar:uploadResults[0].secure_url,
+                avatarCloudinaryPublicId:uploadResults[0].public_id
+            }
+        })
+
+        const secureUserInfo = {
+            id:user.id,
+            name:user.name,
+            username:user.username,
+            avatar:user.avatar,
+            email:user.email,
+            createdAt:user.createdAt,
+            updatedAt:user.updatedAt,
+            emailVerified:user.emailVerified,
+            publicKey:user.publicKey,
+            notificationsEnabled:user.notificationsEnabled,
+            verificationBadge:user.verificationBadge,
+            fcmToken:user.fcmToken,
+            oAuthSignup:user.oAuthSignup
+        }
+        return res.status(200).json(secureUserInfo)
+    }
+)
+
+export const updateNotifications = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
 
     const {isEnabled}:notificationsSchemaType = req.body
 
-    const user = await User.findByIdAndUpdate(req.user?._id,{notificationsEnabled:isEnabled},{new:true})
-
-    return res.status(200).json({notificationsEnabled:user?.notificationsEnabled})
-
+    const user = await prisma.user.update({
+        where:{
+            id:req.user.id
+        },
+        data:{
+            notificationsEnabled:isEnabled
+        }
+    })
+    return res.status(200).json({notificationsEnabled:user.notificationsEnabled})
 })
 
-const testEmailHandler = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
+export const testEmailHandler = asyncErrorHandler(async(req:AuthenticatedRequest,res:Response,next:NextFunction)=>{
 
     const {emailType} = req.query
 
     if(emailType === 'welcome'){
-        await sendMail(req.user?.email!,req.user?.username!,'welcome',undefined,undefined,undefined)
+        await sendMail(req.user.email,req.user.username,'welcome',undefined,undefined,undefined)
         return res.status(200).json({message:`sent ${emailType}`})
     }
 
     if(emailType==='resetPassword'){
-        await sendMail(req.user?.email!,req.user?.username!,'resetPassword','https://baatchit.online',undefined,undefined)
+        await sendMail(req.user.email,req.user.username,'resetPassword','https://mernchat.online',undefined,undefined)
         return res.status(200).json({message:`sent ${emailType}`})
     }
 
     if(emailType==='otpVerification'){
-        await sendMail(req.user?.email!,req.user?.username!,'OTP',undefined,"3412",undefined)
+        await sendMail(req.user.email,req.user.username,'OTP',undefined,"3412",undefined)
         return res.status(200).json({message:`sent ${emailType}`})
     }
     if(emailType==='privateKeyRecovery'){
-        await sendMail(req.user?.email!,req.user?.username!,'privateKeyRecovery',undefined,undefined,'https://baatchit.online')
+        await sendMail(req.user.email,req.user.username,'privateKeyRecovery',undefined,undefined,'https://mernchat.online')
         return res.status(200).json({message:`sent ${emailType}`})
     }
-
-    res.status(200).json({})
+    res.status(200)
 })
 
-export { getUserDetails ,getUserByUsername, udpateUser, updateNotifications , testEmailHandler};
