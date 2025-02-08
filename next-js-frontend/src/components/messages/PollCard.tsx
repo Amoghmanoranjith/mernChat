64f34a23216b8dd5fa31f56ca5edb5605e8997bc
rishibakshi <guitarguyrishi@gmail.com>
2025-02-08 20:52:50 +0530

    chore: improve ui of poll-card

--- next-js-frontend/src/components/messages/PollCard.tsx
+++ next-js-frontend/src/components/messages/PollCard.tsx
@@ -32,6 +32,7 @@ export const PollCard = ({ pollData, messageId}: PropTypes) => {
         isMultipleAnswers={pollData.multipleAnswers}
         optionIndexToVotesMap={optionIndexToVotesMap}
       />
+      <span className="bg-white w-full h-[1px]"/>
       <button onClick={e=>{e.preventDefault(); e.stopPropagation(); handleViewVotesClick()}} className="text-center">
         View votes
       </button>
